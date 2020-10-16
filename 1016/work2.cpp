af2b86bb98657fd680726fea3f92f7b3fdcaf429
이지연 <jjj000223@cu.ac.kr>
2020-10-16 11:50:58 +0900

    초기 커밋.

---
+++ 1016/work2.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n > 0)
+        return n * factorial(n - 1);
+    else
+        return 1;
+}
+int main(void)
+{
+    int x;
+    printf("정수를 입력하세요 : ");
+    scanf("%d", &x);
+    printf("%d의 순차곱셈 값은 %d입니다.\n", x, factorial(x));
+
+    return 0;
+}
